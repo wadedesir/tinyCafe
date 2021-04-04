1e9c7939b9e6debe9efba833013578982ea62e10
Wade Desir <wededesir@gmail.com>
2021-04-03 21:45:23 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+    'url' : "mongodb+srv://dashlin:sermeil15@dashbase.olf7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    'dbName': 'database'
+};
+
+/*
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
+*/
