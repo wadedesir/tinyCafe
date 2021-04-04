ba33e94d43505a6535fa85d8345d3a4947ab8e65
Wade Desir <wededesir@gmail.com>
2021-04-03 23:56:45 -0400

    complete

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
-    'url' : "mongodb+srv://dashlin:sermeil15@dashbase.olf7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    'url' : "mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     'dbName': 'database'
 };
 
